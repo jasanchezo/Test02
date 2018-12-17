fc6aacba636969134dca5e86dd98f8eb59bd276a
José Alejandro Sánchez Ortega <jasanchezo@gmail.com>
2018-12-17 15:16:54 -0600

    Inserción inicial en el repositorio del módulo Test02

---
+++ Test02.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Test02.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
